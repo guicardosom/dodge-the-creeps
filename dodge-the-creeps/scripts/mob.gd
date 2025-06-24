8aa38f23e809fde81a218c888607cb4275d8b341
guicardoso <gccardosomiranda@outlook.com>
2025-06-24 13:07:53 -0400

    Abstracts mobs initialization

--- dodge-the-creeps/scripts/mob.gd
+++ dodge-the-creeps/scripts/mob.gd
@@ -5,5 +5,17 @@ func _ready():
 	$AnimatedSprite2D.animation = mob_types.pick_random()
 	$AnimatedSprite2D.play()
 
-func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
+
+func initialize(spawn_position: Vector2, base_rotation: float):
+	# Set the mob's position
+	position = spawn_position
+
+	# Set direction with randomness
+	var direction = base_rotation + PI / 2 + randf_range(-PI / 4, PI / 4)
+	rotation = direction
+
+	# Assign a random velocity rotated in the direction
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	linear_velocity = velocity.rotated(direction)

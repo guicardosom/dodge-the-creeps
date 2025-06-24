a1d111e5d074e04459a1e10c7031859c79863c9f
guicardoso <gccardosomiranda@outlook.com>
2025-06-24 12:26:07 -0400

    Concludes first game!

--- dodge-the-creeps/scripts/player.gd
+++ dodge-the-creeps/scripts/player.gd
@@ -36,13 +36,12 @@ func _process(delta):
 		$AnimatedSprite2D.animation = "up"
 		$AnimatedSprite2D.flip_v = velocity.y > 0
 
-
 func _on_body_entered(body: Node2D) -> void:
 	hide() # Player disappears after being hit.
 	hit.emit()
 	# Must be deferred as we can't change physics properties on a physics callback.
 	$CollisionShape2D.set_deferred("disabled", true)
-	
+
 func start(pos):
 	position = pos
 	show()
